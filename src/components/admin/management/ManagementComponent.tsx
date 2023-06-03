ded21bd30151a47cfb3dd01e25506cfc6c52508d
KimSungJin <rainbow96bear@gmail.com>
2023-06-03 23:37:41 +0900

    db에서 목록 불러와 리스트화 초안

--- src/components/admin/management/ManagementComponent.tsx
+++ src/components/admin/management/ManagementComponent.tsx
@@ -1,5 +1,32 @@
-const MangementComponent = () => {
-  return <div></div>;
+import { styled } from "styled-components";
+
+type Props = {
+  list: any;
+};
+
+const MangementComponent: React.FC<Props> = ({ list }) => {
+  console.log(list);
+  return (
+    <div>
+      {list?.map((item: any, index: number) => (
+        <ListTable key={`item-${index}`}>
+          <ul>{item.productName}</ul>
+          <ul>{item.productKind}</ul>
+          <ul>{item.price}</ul>
+          <ul>{item.info}</ul>
+        </ListTable>
+      ))}
+    </div>
+  );
 };
 
 export default MangementComponent;
+
+const ListTable = styled("div")({
+  display: "flex",
+  justifyContent: "space-between",
+  "& ul": {
+    // padding: "auto",
+    border: "1px solid red",
+  },
+});
--- src/components/admin/management/ManagementContainer.tsx
+++ src/components/admin/management/ManagementContainer.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import QueryString from "qs";
+
 import MangementComponent from "./ManagementComponent";
 
 const MangementContainer = () => {
-  return <MangementComponent></MangementComponent>;
+  const [list, setList] = useState([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    const queryData = QueryString.parse(location.search, {
+      ignoreQueryPrefix: true,
+    });
+
+    const getList = async () => {
+      const result = await axios.get("/api/productList/getList", {
+        params: queryData,
+      });
+      setList(result.data);
+    };
+
+    getList();
+  }, [location.search]);
+
+  return <MangementComponent list={list}></MangementComponent>;
 };
 
 export default MangementContainer;
