0f92656380ee748a02dfc43207d36865a3ddc196
KimSungJin <rainbow96bear@gmail.com>
2023-07-09 00:06:14 +0900

    밑줄과 색상이 없는 Link 태그

---
+++ src/customComponent/SimpleLink.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const SimpleLink = styled(Link)({
+  textDecoration: "none",
+  cursor: "pointer",
+  color: "inherit",
+});
+
+export default SimpleLink;
